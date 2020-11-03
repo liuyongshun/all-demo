291f6579a12e498e8adc118ecfca2bb91d0e91e9
dandan <liuyongshun2@126.com>
2020-11-03 08:37:28 +0800

    save

--- cache/index.js
+++ cache/index.js
@@ -1,46 +1,30 @@
-// jsonp 实现方式 ==================================
 const Koa =require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const router = new Router();
+const static = require('koa-static');
+const path = require('path');
+// const conditional = require('koa-conditional-get');
+const etag = require('koa-etag');
 
 const app = new Koa();
-const cors = require('koa2-cors');
 
-app.use(cors({
-  origin: function () {
-    return '*';
-  },
-  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-  maxAge: 5,
-  credentials: true,
-  allowMethods: ['GET', 'POST', 'DELETE', 'OPTION', 'PUT'],
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Length', 'credentials', 'X-Access-Token', 'Cache-Control', 'Pragma', 'X-Requested-With']
-}));
+const staticPath = './static'
 
-
-const home = router.get('/v1/home', async (ctx) => {
-    console.log(ctx.request)
-    ctx.response.body = {
-        code: 0,
-        data: {
-            name: 'liu',
-            age: 11
+// app.use(conditional())
+app.use(etag())
+app.use(
+    static(
+        path.join( __dirname,  staticPath),
+        {
+            maxage: 5 * 1000
         }
-    }
-});
-
-router.use(home.routes())
-
-app.use(bodyParser());
-app.use(router.routes())
+    )
+)
 
 // 错误处理
 app.on('error', err => {
     console.log('服务错误', err)
 });
 
-app.listen(6666, '0.0.0.0', () => {
-    console.log('启动成功！端口：6666');
+app.listen(3388, '0.0.0.0', () => {
+    console.log('启动成功！端口：3388');
 });
 
